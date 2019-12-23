1407b22f4045f205cfca695d5f34ceae79477393
Bart Teunis <bart_teunis@hotmail.com>
2019-12-23 15:41:41 +0100

    First version with code, still needs some fixing here and there

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # blender-gms-sprite
 Export a top-down render of the current model as a sprite resource for GameMaker:Studio
+
+## Use
+In a Blender file, add a scene and a camera to that scene.
+Set it to orthographic and facing downward.
+Add the model that you want to render to the scene and make it the active object.
+Run the plugin with the required settings.
--- io_export_gms_sprite.py
+++ io_export_gms_sprite.py
@@ -8,15 +8,16 @@ from bpy.types import Operator
 
 
 class ExportGMSSprite(Operator, ExportHelper):
-    """This appears in the tooltip of the operator and in the generated docs"""
+    """Export a top-down render of the current model as a sprite resource for GameMaker:Studio"""
     bl_idname = "export_gms.sprite"
-    bl_label = "Export GM:Studio Sprite"
+    bl_label = "GM:Studio Sprite"
+    bl_options = {'PRESET'}
 
     # ExportHelper mixin class uses this
-    filename_ext = ".yy"
+    filename_ext = ".png"
 
     filter_glob = StringProperty(
-            default="*.yy",
+            default="*.png",
             options={'HIDDEN'},
             maxlen=255,  # Max internal buffer length, longer would be clamped.
             )
@@ -55,21 +56,74 @@ class ExportGMSSprite(Operator, ExportHelper):
         default='OPT_A',
         )
     
-    def write_some_data(self, context):
-        print("running write_some_data...")
-        f = open(self.filepath, 'w', encoding='utf-8')
-        f.write("Hello World %s" % self.tile_horizontally)
-        f.close()
-
-        return {'FINISHED'}
-
     def execute(self, context):
-        return self.write_some_data(context)
+        fname = self.filepath
+
+        # This'll be our object and render camera
+        obj = context.object
+        obj_cam = context.scene.camera
+        cam = obj_cam.data
+
+        # Origin calculations based on cursor location and mesh bounding box
+        pos = obj.location
+        dim = obj.dimensions
+        top_right = (pos + dim/2)[0:2]
+        bottom_left = (pos - dim/2)[0:2]
+
+        cur_loc = context.scene.cursor_location[0:2]
+
+        max_x, max_y = max(top_right, bottom_left)
+        min_x, min_y = min(top_right, bottom_left)
+
+        print("----------------")
+        print(cur_loc)
+
+        mac_x, mac_y = max(max_x, cur_loc[0]), max(max_y, cur_loc[1])
+        mic_x, mic_y = min(min_x, cur_loc[0]), min(min_y, cur_loc[1])
+
+        print(mic_x, mic_y)
+        print(mac_x, mac_y)
+
+        cam_x = mic_x + (mac_x-mic_x)/2
+        cam_y = mic_y + (mac_y-mic_y)/2
+
+        print("Cam:")
+        print(cam_x, cam_y)
+
+        origin_x = cur_loc[0] - mic_x
+        origin_y = cur_loc[1] - mic_y
+
+        print("Origin:")
+        print(origin_x, origin_y)
+
+        w = mac_x - mic_x
+        h = mac_y - mic_y
+
+        # Now position camera and resize canvas
+        r = context.scene.render
+        if w >= h:
+            cam.ortho_scale = w
+            #r.resolution_x = w
+            r.resolution_y = h/w*r.resolution_x
+        else:
+            cam.ortho_scale = h
+            #r.resolution_y = h
+            r.resolution_x = w/h*r.resolution_y
+
+        obj_cam.location = (cam_x, cam_y, 0)
+        obj_cam.location.z = obj.dimensions.y * .5
+
+        # Render the thing
+        bpy.ops.render.render()
+
+        bpy.data.images['Render Result'].save_render(fname)
+        
+        return {'FINISHED'}
 
 
 # Only needed if you want to add into a dynamic menu
 def menu_func_export(self, context):
-    self.layout.operator(ExportGMSSprite.bl_idname, text="Export GM:Studio Sprite")
+    self.layout.operator(ExportGMSSprite.bl_idname, text="GM:Studio Sprite (*.yy)")
 
 
 def register():
